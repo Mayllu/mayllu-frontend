477b18cda7cdbe498042ace500874fef83cb884d
Kalos Lazo <118573214+kaloslazo@users.noreply.github.com>
2024-11-19 18:54:03 -0500

    feat: eslint fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: [
-    "expo",
-    "prettier",
-  ],
+  extends: ["expo", "prettier"],
   plugins: ["prettier"],
   rules: {
     "prettier/prettier": "error",
